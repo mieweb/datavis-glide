a8f1293b43559673f0f54aa1c36c1e3398d8e387
Taylor Venable <tvenable@mieweb.com>
2019-08-14 17:05:34 -0400

    Dev: Update eslint configuration.

---
+++ tests/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true,
+    "mocha": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 8,
+    "sourceType": "module"
+  },
+  "extends": "eslint:recommended"
+};
