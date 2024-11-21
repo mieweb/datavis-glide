5e586cf43409413637df5492013f3f9bf1bcb9bc
Taylor Venable <tvenable@mieweb.com>
2024-11-21 16:01:45 -0500

    Dev: jQuery and jQuery UI are now also bundled when using DataVis as a dependency via NPM.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import jQuery from 'jquery';
+
+// Don't try to inline this code, it won't work. Imports are lifted,
+// and this code needs to run before we import jQuery UI. Yes, this
+// is an unhinged workaround.
+
+import original_jQuery from './global-jquery.js';
+
 import 'block-ui';
 import 'flatpickr';
 import 'jquery-ui/dist/jquery-ui.min.js';
@@ -11,6 +19,17 @@ import { Grid } from './src/grid.js';
 import { Graph } from './src/graph.js';
 import { Prefs } from './src/prefs.js';
 import { Perspective } from './src/perspective.js';
+
+// We left the global jQuery around long enough for jQuery UI to install itself, and that same
+// jQuery object has been used by all other plugins and DataVis code.  Now that we're all done,
+// make it so nobody can access our jQuery, to avoid conflicts.
+
+if (original_jQuery != null) {
+  window.jQuery = original_jQuery;
+}
+else {
+  delete window.jQuery;
+}
 
 export {
   Source,
